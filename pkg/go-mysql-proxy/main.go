8ab7b8645c4b42924fb1f5f1ca975757f7fd2ec8
Vladimir <56608878+meowgen@users.noreply.github.com>
2022-10-17 10:07:15 +0500

    Add files via upload

---
+++ pkg/go-mysql-proxy/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	proxy2 "go-mysql-proxy/proxy"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	proxy := proxy2.NewProxy("192.168.0.12", ":3306", ctx)
+	proxy.EnableDecoding()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("Signal received %v, stopping and exiting...", sig)
+			cancel()
+		}
+	}()
+
+	err := proxy.Start("3336")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
